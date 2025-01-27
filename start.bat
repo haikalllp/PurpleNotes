dde410dcd7f1859faeb9a6014bc93b18fc673c25
shrewdy <swfox24@gmail.com>
2025-01-28 01:37:31 +0800

    fix bat

--- start.bat
+++ start.bat
@@ -19,6 +19,13 @@ if %errorlevel% equ 0 (
     exit /b
 )
 
+where brave.exe >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Opening in Brave app mode...
+    start "" brave.exe --app=http://localhost:5500
+    exit /b
+)
+
 where msedge.exe >nul 2>&1
 if %errorlevel% equ 0 (
     echo Opening in Edge app mode...
@@ -26,5 +33,13 @@ if %errorlevel% equ 0 (
     exit /b
 )
 
+where opera_gx.exe >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Opening in Opera GX app mode...
+    start "" opera_gx.exe --app=http://localhost:5500
+    exit /b
+)
+
+echo No supported browsers found for app mode.
 echo Opening in default browser...
 start "" http://localhost:5500
